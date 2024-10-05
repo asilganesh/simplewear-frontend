5b1b6462c4c845a51907564bc41ae51577693e73
ganeshasil <ganeshasil464@gmail.com>
2024-10-05 20:56:07 +0530

    Modified the Store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./productReducer";
 import { cartReducer } from "./cartStore";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = combineReducers({
+    cartReducer,
+    productReducer
+})
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 const store = configureStore({
 
-    reducer:{
-        productReducer,
-        cartReducer
-    }
+    reducer:persistedReducer,
+    middleware: (getDefaultMiddleWare) =>  getDefaultMiddleWare({
+            serializableCheck: false,
+          }),
 })
 
-export default store
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './Redux/store.js'
+import {persistor, store} from './Redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 
 createRoot(document.getElementById('root')).render(
  
     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
     </Provider>
 
 )
