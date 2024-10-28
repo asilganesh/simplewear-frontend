997aa8f52754ad9a2a8dfce7e9eed9ee361af775
ganeshasil <ganeshasil464@gmail.com>
2024-10-28 16:05:30 +0530

    created thunk function to update order status

--- src/Redux/myOrdersStore.js
+++ src/Redux/myOrdersStore.js
@@ -1,16 +1,16 @@
 import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
 import smStates from "../utils/sm_states/index"
-import { addOrdersAsync, fetchAllOrdersAsync, fetchOrdersAsync } from "../api/orders";
+import { addOrdersAsync, fetchAllOrdersAsync, fetchOrdersAsync, updateOrdersAsync } from "../api/orders";
 
 
-export const getOrders = createAsyncThunk('fetchOrders', async(userId) => {
+export const getOrders = createAsyncThunk('fetchOrders', async (userId) => {
 
     const data = await fetchOrdersAsync(userId)
 
     return data
 })
 
-export const getAllOrders = createAsyncThunk('fetchAllOrders', async() => {
+export const getAllOrders = createAsyncThunk('fetchAllOrders', async () => {
 
     const data = await fetchAllOrdersAsync()
 
@@ -18,18 +18,20 @@ export const getAllOrders = createAsyncThunk('fetchAllOrders', async() => {
 })
 
 
-export const addToMyOrders = createAsyncThunk('addToMyOrdersList', async(ordersData) => {
+export const addToMyOrders = createAsyncThunk('addToMyOrdersList', async (ordersData) => {
     const data = await addOrdersAsync(ordersData)
     return data
 })
 
-export const updateOrderStatus = createAsyncThunk('updateOrderStatus', async(orderStatus) => {
+export const updateOrderStatus = createAsyncThunk('updateOrderStatus', async (orderStatus) => {
 
-    const newstatus = {
+    const newStatus = {
         _id: orderStatus.itemId,
         orderStatus: orderStatus.status
     }
-    debugger;
+
+    const data = await updateOrdersAsync(newStatus)
+    return data
 })
 
 const initialState = {
@@ -39,71 +41,68 @@ const initialState = {
     status: smStates.IS_IDEAL
 }
 
-export const myOrdersReducer = createReducer(initialState,(builder) => {
-
-
-    
+export const myOrdersReducer = createReducer(initialState, (builder) => {
 
     builder
-    .addCase(getOrders.pending, (state) => {
-        state.loading =true,
-        state.status= smStates.IS_TRIGGERED
-    })
+        .addCase(getOrders.pending, (state) => {
+            state.loading = true,
+                state.status = smStates.IS_TRIGGERED
+        })
 
-    .addCase(getOrders.fulfilled, (state,action) => {
-        state.loading = false,
-        state.orders= action.payload.data.data
-        state.status = smStates.IS_SUCCESSFUL
-        if(!action.payload) {
-            state.status = smStates.IS_SUCCESS_BUT_NO_DATA
-        }
-    })
+        .addCase(getOrders.fulfilled, (state, action) => {
+            state.loading = false,
+                state.orders = action.payload.data.data
+            state.status = smStates.IS_SUCCESSFUL
+            if (!action.payload) {
+                state.status = smStates.IS_SUCCESS_BUT_NO_DATA
+            }
+        })
 
-    .addCase(getOrders.rejected, (state,action) => {
+        .addCase(getOrders.rejected, (state, action) => {
 
-        state.loading= false,
-        state.error= action.error.message,
-        state.status= smStates.IS_FAILED
-    })
-    
-    .addCase(getAllOrders.pending, (state) => {
-        state.loading =true,
-        state.status= smStates.IS_TRIGGERED
-    })
+            state.loading = false,
+                state.error = action.error.message,
+                state.status = smStates.IS_FAILED
+        })
 
-    .addCase(getAllOrders.fulfilled, (state,action) => {
-        state.loading = false,
-        state.orders= action.payload.data.data
-        state.status = smStates.IS_SUCCESSFUL
-        if(!action.payload) {
-            state.status = smStates.IS_SUCCESS_BUT_NO_DATA
-        }
-    })
+        .addCase(getAllOrders.pending, (state) => {
+            state.loading = true,
+                state.status = smStates.IS_TRIGGERED
+        })
 
-    .addCase(getAllOrders.rejected, (state,action) => {
+        .addCase(getAllOrders.fulfilled, (state, action) => {
+            state.loading = false,
+                state.orders = action.payload.data.data
+            state.status = smStates.IS_SUCCESSFUL
+            if (!action.payload) {
+                state.status = smStates.IS_SUCCESS_BUT_NO_DATA
+            }
+        })
 
-        state.loading= false,
-        state.error= action.error.message,
-        state.status= smStates.IS_FAILED
-    })
+        .addCase(getAllOrders.rejected, (state, action) => {
 
-    .addCase(addToMyOrders.pending,  (state) => {
-        state.loading = true;
-        state.status = smStates.IS_TRIGGERED;
-    })
-    .addCase(addToMyOrders.fulfilled,(state,action) => {
-        state.loading = false;
+            state.loading = false,
+                state.error = action.error.message,
+                state.status = smStates.IS_FAILED
+        })
+
+        .addCase(addToMyOrders.pending, (state) => {
+            state.loading = true;
+            state.status = smStates.IS_TRIGGERED;
+        })
+        .addCase(addToMyOrders.fulfilled, (state, action) => {
+            state.loading = false;
             state.orders = action.payload.data
             state.status = smStates.IS_SUCCESSFUL;
 
-            if(!action.payload){
+            if (!action.payload) {
                 state.status = smStates.IS_SUCCESS_BUT_NO_DATA
             }
-            
-    })
-    .addCase(addToMyOrders.rejected, (state,action) => {
+
+        })
+        .addCase(addToMyOrders.rejected, (state, action) => {
             state.loading = false;
             state.error = action.error.message;
             state.status = smStates.IS_FAILED;
-    })
+        })
 })
